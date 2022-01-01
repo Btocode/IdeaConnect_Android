cdd2c73b879770193310ffca4f250e761f878fc2
Afsan Saeed <afsansaeed2017@gmail.com>
2022-01-01 18:22:30 +0600

    Login Page

--- app/src/main/java/com/afsan/ideaconnect_android/MainActivity.java
+++ app/src/main/java/com/afsan/ideaconnect_android/MainActivity.java
@@ -6,27 +6,75 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.auth0.android.jwt.Claim;
+import com.auth0.android.jwt.JWT;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavView;
-
-
-    @SuppressLint("NonConstantResourceId")
+    String access,refresh;
+    SharedPreferences sp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavView = findViewById(R.id.bottom_nav_menu);
+
+
+
+
+        Intent iin= getIntent();
+        Bundle b = iin.getExtras();
+        String status = "";
+        SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+        String name = sp.getString("name","");
+        System.err.println("Printing from Main page "+name + "=<<");
+
+
+
+//        if(b!=null){
+//            status = (String) b.get("stat");
+//        }
+//        if (status == ""){
+//            if(b!=null)
+//            {
+//                access =(String) b.get("access");
+//                refresh =(String) b.get("refresh");
+//            }
+//            JWT jwt = new JWT(access);
+//            Claim name = jwt.getClaim("username");
+//            Claim id = jwt.getClaim("user_id");
+//            String username = name.asString();
+//            int userid = id.asInt();
+//
+//            sp = PreferenceManager.getDefaultSharedPreferences(this);
+//            SharedPreferences.Editor editor = sp.edit();
+//            editor.putString("name", username);
+////        editor.putInt("id", userid);
+//            editor.commit();
+//            Toast.makeText(MainActivity.this,"Saved into preference", Toast.LENGTH_LONG).show();
+//
+//        }
+
+//        System.out.println("UserName : "+username);
+//        System.out.println("UserId : "+userid);
+
+
+
+
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container,new Home());
         transaction.commit();
@@ -52,7 +100,9 @@ public class MainActivity extends AppCompatActivity {
             }
             if (fragment == null) return true;
             MainActivity.this.getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment).commit();
+
             return true;
+
         });
 
     }
--- app/src/main/java/com/afsan/ideaconnect_android/Model/UserInfo.java
+++ app/src/main/java/com/afsan/ideaconnect_android/Model/UserInfo.java
@@ -1,11 +1,15 @@
 package com.afsan.ideaconnect_android.Model;
 
 public class UserInfo {
-    private String firstName, lastName;
+    private String firstName, lastName, email, password,gender, age;
 
-    public UserInfo(String firstName, String lastName) {
+    public UserInfo(String firstName, String lastName, String email, String password, String gender, String age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.gender = gender;
+        this.age = age;
     }
 
     public String getFirstName() {
@@ -22,5 +26,37 @@ public class UserInfo {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
     }
 }
